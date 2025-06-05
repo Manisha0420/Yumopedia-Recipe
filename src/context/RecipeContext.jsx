6e576e3702d35949c3af0a306763f45fa2e76bfd
Manisha0420 <manishawdn4@gmail.com>
2025-06-05 17:34:49 +0530

    4th commit

--- src/context/RecipeContext.jsx
+++ src/context/RecipeContext.jsx
@@ -1,11 +1,81 @@
 import React, { createContext, useContext, useState } from 'react'
-
-
 // 1. Context object banaya gaya
 const recipeContext = createContext(null);
 // 2. provider Context
 const RecipeContext = (props) => {
-  const [data , setData] = useState([]);
+  const [data , setData] = useState([
+     {
+      id: 1,
+      title: "Classic Margherita Pizza",
+      ingredients: [
+        "Pizza dough",
+        "Tomato sauce",
+        "Fresh mozzarella cheese",
+        "Fresh basil leaves",
+        "Olive oil",
+        "Salt and pepper to taste"
+      ],
+      instructions: [
+        "Preheat the oven to 475°F (245°C).",
+        "Roll out the pizza dough and spread tomato sauce evenly.",
+        "Top with slices of fresh mozzarella and fresh basil leaves.",
+        "Drizzle with olive oil and season with salt and pepper.",
+        "Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.",
+        "Slice and serve hot."
+      ],
+      image: "https://cdn.dummyjson.com/recipe-images/1.webp",
+      chef: "Aman",
+      desc: "Preheat the oven to 475°F (245°C), Bake in the preheated oven for 12-15 minutes or until the crust is golden brown."
+    },
+    {
+      id: 2,
+      title: "Spicy Veg Hakka Noodles",
+      ingredients: [
+        "Hakka noodles",
+        "Carrot (julienned)",
+        "Capsicum (sliced)",
+        "Cabbage (shredded)",
+        "Soy sauce",
+        "Chili sauce",
+        "Garlic, ginger",
+        "Salt and pepper"
+      ],
+      instructions: [
+        "Boil noodles as per instructions and set aside.",
+        "Heat oil in a wok, sauté garlic and ginger.",
+        "Add vegetables and stir-fry on high flame.",
+        "Add soy sauce, chili sauce, salt, and pepper.",
+        "Toss in noodles and mix well.",
+        "Serve hot with chili vinegar."
+      ],
+      image: "https://cdn.dummyjson.com/recipe-images/2.webp",
+      chef: "Ritika",
+      desc: "Quick Indo-Chinese dish full of flavors and vegetables, perfect for a spicy evening snack or meal."
+    },
+    {
+      id: 3,
+      title: "Paneer Butter Masala",
+      ingredients: [
+        "Paneer cubes",
+        "Tomato puree",
+        "Cream",
+        "Butter",
+        "Ginger-garlic paste",
+        "Spices (garam masala, chili powder)",
+        "Salt"
+      ],
+      instructions: [
+        "Heat butter in a pan and sauté ginger-garlic paste.",
+        "Add tomato puree and cook till oil separates.",
+        "Add spices and mix well.",
+        "Add cream and paneer, cook for 5-7 minutes.",
+        "Garnish with coriander and serve hot."
+      ],
+      image: "https://cdn.dummyjson.com/recipe-images/3.webp",
+      chef: "Manoj",
+      desc: "Creamy North Indian curry made with paneer and rich tomato-based gravy."
+    }
+  ]);
   return (
     <recipeContext.Provider value={{data ,setData}}>
       {props.children}
--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -1,11 +1,134 @@
 import React from 'react'
+import { useParams } from 'react-router'
+import { useRecipe } from '../context/RecipeContext';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 const SingleRecipe = () => {
-  return (
-    <div>
-      
-    </div>
+  const {id} =useParams();
+  const {data ,setData} = useRecipe();
+  const recipe = data.find((r)=> r.id == id);
+  const {
+         register,
+         handleSubmit, 
+         reset ,
+         formState:{errors}
+        } =useForm({
+          defaultValues:{
+            image: recipe.image,
+            title: recipe.title,
+            chef: recipe.chef,
+            desc: recipe.desc,
+            ingr: recipe.ingr,
+            inst: recipe.inst,
+          }
+        });
+
+        const SubmitHandler = (updatedRecipe) =>{
+        const i = data.findIndex((r) => r.id == id);
+        // code to update recipe
+        console.log(data[i]);
+        const copydata = [...data];
+        console.log(data)
+        copydata[i] = { ...recipe, ...updatedRecipe };
+
+        setData(copydata);
+        
+        toast.success("recipe updated!");
+        reset();
+        
+        }
+
+        const DeleteHandler = () =>{
+        
+        }
+
+  return recipe ? (
+    <div className="mt-5 w-full h-screen overflow-auto flex  ">
+            <div className="left w-[50vw]">
+                <h1 className="text-5xl font-black">{recipe.title}</h1>
+                <img
+                    className="h-[30vh] object-cover"
+                    src={recipe.image}
+                    alt=""
+                />
+                <small className=" font-black text-red-400">
+                    {recipe.chef}
+                </small>
+            </div>
+             <form
+                onSubmit={handleSubmit(SubmitHandler)}
+                className="text-xl mt-5 p-5 shadow w-1/2"
+            >
+                <input
+                    className="w-full mb-5 block border-b p-1 outline-0"
+                    {...register("image")}
+                    type="url"
+                    placeholder="Image url"
+                />
+                {errors.image && <small className='text-red-400' >
+                  Image is Required
+                  </small>}
+
+                <input
+                    className="w-full mb-5 block border-b p-1 outline-0"
+                    {...register("title")}
+                    type="text"
+                    placeholder="Title"
+                />
+                <input
+                    className="w-full mb-5 block border-b p-1 outline-0"
+                    {...register("chef")}
+                    type="text"
+                    placeholder="Chef"
+                />
+
+                <textarea
+                    className="w-full mb-5 block border-b p-1 outline-0"
+                    {...register("desc")}
+                    placeholder="recipe description..."
+                ></textarea>
+                <textarea
+                    className="w-full mb-5 block border-b p-1 outline-0"
+                    {...register("ingr")}
+                    placeholder="recipe ingredients, seperated by comma"
+                ></textarea>
+                <textarea
+                    className="w-full mb-5 block border-b p-1 outline-0"
+                    {...register("inst")}
+                    placeholder="recipe instructions, seperated by comma"
+                ></textarea>
+
+                <select
+                    className="w-full mb-5 block border-b p-1 outline-0"
+                    {...register("category")}
+                >
+                    <option value="breakfast">Breakfast</option>
+                    <option value="lunch">Lunch</option>
+                    <option value="supper">Supper</option>
+                    <option value="dinner">Dinner</option>
+                </select>
+
+                <button
+                    type="submit"
+                    className="bg-green-400 px-4 py-2 rounded"
+                >
+                    Update Recipe
+                </button>
+                <button
+                    onClick={DeleteHandler}
+                    type="button"
+                    className="ml-5 bg-red-400 px-4 py-2 rounded"
+                >
+                    Delete Recipe
+                </button>
+            </form>
+            {/*  */}
+        </div>
+    ) : (
+        "Loading"
   )
+
 }
 
 export default SingleRecipe
